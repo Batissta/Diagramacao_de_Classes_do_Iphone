ab17e400c888f844791c73b1fbe85df6d820cc0c
Batista <140225853+Batissta@users.noreply.github.com>
2023-09-15 13:20:35 -0300

    Create NavegadorDeInternetInterface.java

---
+++ src/navegadorInternet/NavegadorDeInternetInterface.java
@@ -0,0 +1,8 @@
+package navegadorInternet;
+
+public interface NavegadorDeInternetInterface {
+	public void pesquisarPagina(String url);
+	public void abrirNovaAba();
+	public void fecharAbaAtual();
+	public void atualizarPagina();
+}
